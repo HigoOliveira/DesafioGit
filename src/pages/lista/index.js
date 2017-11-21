80f69643e082e9782d9335fbf5fc30f078eb6c39
Higo Ribeiro <HORBJN@gmail.com>
2017-11-20 23:05:00 -0200

    Adicionado o serviço de requisição

--- src/pages/lista/index.js
+++ src/pages/lista/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text,
 } from 'react-native';
+
+import api from 'services/api';
 
 import Header from './components/header';
 import Repository from './components/Repository';
+
 
 import styles from './styles';
 
@@ -13,6 +15,13 @@ export default class Lista extends Component {
   static navigationOptions = {
     header: props => <Header {...props} />,
   }
+  async componentWillMount() {
+    const response = await api.get('repos/rocketseat/rocketseat.com.br');
+    const { name, full_name, organization: { avatar_url }, description } = response.data;
+    console.tron.log(avatar_url);
+
+  }
+
   render() {
     return (
       <View>
